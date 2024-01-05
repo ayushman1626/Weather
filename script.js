fe7960da9834eb284dda8ac00ae6781646194dd7
ayushman1626 <ayushmanpal1626@gmail.com>
2024-01-05 21:54:37 +0530

    added lacation feture

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector(".searchbar");
 const btn = document.querySelector(".btn");
+const locationBtn = document.querySelector(".locationBtn");
 const weatherImg = document.querySelector(".icon");
 const temp = document.querySelector(".temp");
 const humidity = document.querySelector(".humidityData");
@@ -13,7 +14,7 @@ const noData = document.querySelector(".noData")
 
 const key = "5120054d17d61e3b50da1d4b3737bded";
 var url = ``;
-
+var lat;var lon;
 // https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid={API key}&units=metric
 
 const imgCollect = {
@@ -39,17 +40,8 @@ function updateDetails(data){
         cityName.innerText = data.name;
 }
 
-// function notFound(error){
-//     hero.classList.remove("before");
-//     hero.classList.add("container");
-//     noData.classList.add("noDataSpec");
-//     noData.innerHTML = "<img src='res/noData.jpg' alt=''>";
-//     mainBody.classList.add("hidden")
-//     console.log(error);
-// }
-
-function getWeather(){
-    const weather = fetch(url);
+function getWeather(url_){
+    const weather = fetch(url_);
     weather.then((response)=>{
         return response.json();
     }).then((data)=>{
@@ -64,7 +56,23 @@ function getWeather(){
 btn.addEventListener("click",()=>{
     url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${key}&units=metric`
     console.log(url);
-    getWeather();
+    getWeather(url);
+})
+
+locationBtn.addEventListener("click",async ()=>{
+    navigator.geolocation.getCurrentPosition((location)=>{
+        console.log(location);
+        lat = location.coords.latitude;
+        lon = location.coords.longitude;
+        console.log(lat);
+        console.log(lon);
+        url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`
+        getWeather(url);
+    },
+    ()=>{
+
+    }
+    )
 })
 
 input.addEventListener("keypress", function(event) {
@@ -74,23 +82,3 @@ input.addEventListener("keypress", function(event) {
       btn.click();
     }
   });
-
-function getRandomColor() {
-    // Generate a random color in hexadecimal format
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
-}
-
-
-// let color2 = getRandomColor();
-// let color1 = getRandomColor();
-// setInterval(()=>{
-//     var color1 = getRandomColor();
-//         document.body.style.background = `linear-gradient(160deg, ${color1}, ${color2})`;
-// },2000)
-
-// setTimeout(() => {
-//     setInterval(()=>{
-//        let color2 = getRandomColor();
-//         document.body.style.background = `linear-gradient(160deg, ${color1}, ${color2})`;
-//     },2000)
-// }, 500);
